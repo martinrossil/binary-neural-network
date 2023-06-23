2f409172687086d9cdaf05133c3e73e63861684f
Martin Rossil <martinrossil@gmail.com>
2023-06-23 11:54:21 +0200

    Created types & interfaces.

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,16 @@
-export type Point = {
+export interface Point {
 	x: number;
 	y: number;
-};
+}
+
+export interface TouchPoint extends Point {
+	offset: number;
+}
+
+export type Ray = [Point, Point];
+
+export type Rays = Ray[];
+
+export type Border = [Point, Point];
+
+export type Borders = [Border, Border];
